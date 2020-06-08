4df067e1aac058f912910281ea98a72833d9d677
Tyler Bates <Fondlytyler@gmail.com>
2020-06-08 16:05:30 -0700

    initial commit for reference.

---
+++ MvcApiCall/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApiCall.Models;
+using System;
+
+namespace MvcApiCall.Controllers
+{
+  public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var allArticles = Article.GetArticles(EnvironmentVariables.ApiKey);
+            
+            return View(allArticles);
+        }
+    }
+}
---
+++ MvcApiCall/Models/EnvironmentalVariables.cs
@@ -0,0 +1,7 @@
+namespace MvcApiCall.Models
+{
+    public static class EnvironmentVariables
+    {
+        public static string ApiKey = "o3X611t7gXBaE20OTk4R08T1Y5IlGAPm";
+    }
+}
